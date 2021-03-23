c7367fe775595205dedf3152750562f445f4a07f
root <root@LAPTOP-QLDHK0FC.localdomain>
2021-03-23 22:01:57 +0530

    route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserdetailComponent } from './userdetail/userdetail.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component: UserdetailComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
